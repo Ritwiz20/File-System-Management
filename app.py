a45dd8da96c8d09d99cf316b5436e21a2f1d00b5
Ritwiz <singhritwiz@gmail.com>
2023-10-25 14:01:04 +0530

    file system management

---
+++ app.py
@@ -0,0 +1,50 @@
+from helper import FileSystem  
+def main():
+    fs = FileSystem("/home/ritwiz/Desktop/commvault")
+
+    while True:
+        print("Options:")
+        print("1. Create File")
+        print("2. Edit File")
+        print("3. Delete File")
+        print("4. Get File Details")
+        print("5. Revert to Version")
+        print("6. Copy File")
+        print("7. Move File")
+        print("8. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            file_name = input("Enter the file name: ")
+            content = input("Enter the file content: ")
+            fs.create_file(file_name, content)
+        elif choice == "2":
+            file_name = input("Enter the file name: ")
+            new_content = input("Enter the new content: ")
+            fs.edit_file(file_name, new_content)
+        elif choice == "3":
+            file_name = input("Enter the file name: ")
+            fs.delete_file(file_name)
+        elif choice == "4":
+            file_name = input("Enter the file name: ")
+            fs.get_details(file_name)
+        elif choice == "5":
+            file_name = input("Enter the file name: ")
+            version_number = int(input("Enter the version number: "))
+            fs.revert_to_version(file_name, version_number)
+        elif choice == "6":
+            file_name = input("Enter the file name: ")
+            new_path = input("Enter the destination path: ")
+            fs.copy_file(file_name, new_path)
+        elif choice == "7":
+            file_name = input("Enter the file name: ")
+            new_path = input("Enter the destination path: ")
+            fs.move_file(file_name, new_path)
+        elif choice == "8":
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+if __name__ == "__main__":
+    main()
---
+++ helper.py
@@ -0,0 +1,121 @@
+import os 
+import shutil
+import datetime
+
+class FileSystem:
+
+    def __init__(self, base_directory):
+        self.base_directory = base_directory
+        self.file_system = {}
+        self.versions = {}
+        self.current_version = 1
+
+        if not os.path.exists(self.base_directory):
+            os.makedirs(self.base_directory)
+
+    
+    def get_details(self, file_name):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+        if os.path.exists(file_path):
+            print("File System : ", self.file_system[file_name])
+            print("Versions : ", self.versions[file_name])
+            print("Current Version : ", self.file_system[file_name]['version'])
+        else:
+            print("File doesn't exist.")
+            
+
+    def create_file(self, file_name, content):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+        
+        if os.path.exists(file_path):
+            print("File already exists.")
+        else:
+            self.file_system[file_name] = {
+                'version': self.current_version,
+                'created_at': datetime.datetime.now()
+            }
+            self.versions[file_name] = {self.current_version: content}
+            self.current_version += 1
+
+            with open(file_path, 'w') as file:
+                file.write(content)
+            print(f"File '{file_name}' created as '{file_path}'.")
+            print(self.file_system)
+            print(self.versions)
+
+    
+    def edit_file(self, file_name, new_content):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+   
+        if os.path.exists(file_path):
+            with open(file_path, 'w') as f:
+                f.write(new_content)
+
+            self.file_system[file_name]['version'] += 1
+            self.file_system[file_name]['created_at'] = datetime.datetime.now()
+            version = self.file_system[file_name]['version']
+            self.current_version = version
+            self.versions[file_name][version] = new_content
+            
+            print(f"File '{file_name}' content has been updated to version {version}.")
+            print(self.file_system)
+            print(self.versions)
+        else:
+            print(f"File '{file_name}' not found in the directory.")
+
+
+    def delete_file(self, file_name):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+        print(file_path)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            del self.file_system[file_name]
+            del self.versions[file_name]
+            self.current_version = 0
+            print(f"Deleted {file_name}.txt successfully!")
+        else:
+            print("No such file found to delete.")
+
+    
+    def revert_to_version(self, file_name, version_number):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+        print(file_path)
+
+        if os.path.exists(file_path):
+            updated_content = self.versions[file_name][version_number]
+            print(updated_content)
+
+            with open(file_path, 'w') as f:
+                f.write(updated_content)
+                print(f"File {file_name} has been reverted to version {version_number}")
+
+            self.file_system[file_name]['version'] = version_number
+            self.file_system[file_name]['created_at'] = datetime.datetime.now()
+        else:
+            print("File not found")
+
+
+    def copy_file(self, file_name, new_path):
+        file_path = os.path.join(self.base_directory, f"{file_name}.txt")
+        print(file_path)
+
+        if os.path.exists(file_path):
+            new_file_path = os.path.join(new_path, f"{file_name}(Copy).txt")
+            shutil.copy(file_path, new_file_path)
+            print(f"{file_name} has been copied to {new_path} with the name {new_file_path.split('/')[-1]}")
+        else:
+            print("File not found which can be copied.") 
+
+
+    def move_file(self, file_name, new_path):
+        file_path = os.path.join(self.base_directory,f"{file_name}.txt")
+        new_file_path = os.path.join(new_path, f"{file_name}.txt")
+
+        if os.path.exists(file_path):
+            if os.path.exists(new_file_path):
+                print(f"File {file_name} already exists in {new_path}")
+            else:
+                shutil.move(file_path, new_file_path)
+                print(f"{file_name} has been copied to {new_path}")
+        else:
+            print("No file found which can be moved")
